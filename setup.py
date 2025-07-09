4613e8b85d9179f70007ebb7940ede1e4464f85e
Deepika Kumar <deepika.kumar.chd@gmail.com>
2025-07-09 18:48:29 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements() -> List[str]:
+
+    requirements_list : List[str] = []
+
+    return requirements_list
+ 
+setup(
+
+    name = "MMSE",
+    version = "0.0.1",
+    author = "Deepika",
+    author_email = "deepika.kumar.chd@gmail.com",
+    packages = find_packages(), 
+    install_requires = get_requirements()
+
+)
